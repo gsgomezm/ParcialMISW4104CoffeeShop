66e5cb5e19684ba4f0b98c520dca09a649a553da
GeraldineGomez <gs.gomezm1@uniandes.edu.co>
2023-11-05 08:14:28 -0500

    Feat: se crea los estilos y logica del componente listar cafes

--- src/app/coffee/coffee.module.ts
+++ src/app/coffee/coffee.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoffeeComponent } from './coffee.component';
+import { CoffeeListComponent } from './coffee-list/coffee-list.component';
 
 
 
 @NgModule({
   declarations: [
-    CoffeeComponent
+    CoffeeComponent,
+    CoffeeListComponent
   ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    CoffeeListComponent
   ]
 })
 export class CoffeeModule { }
