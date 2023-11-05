6314a2fbfd5e56a4903b2dbc06e8656c89665260
GeraldineGomez <gs.gomezm1@uniandes.edu.co>
2023-11-05 08:12:21 -0500

    Revert "Feat: se crea el servicio del cafe"
    This reverts commit c89749d66a96f9c61b7b55d11ec27818a1ed7464.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'https://gist.githubusercontent.com/josejbocanegra/e9d24db370ce95b75555f7d1f8691805/raw/8a26ac2bca4183dc88545e14c45851d698911358/202212_MISW4104_Grupo3.json'
+  baseUrl: 'http://localhost:3000/api/v1/'
 };
 
 /*
